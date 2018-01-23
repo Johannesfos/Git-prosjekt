3bbe8f2f6582c81028524804f7414aa54f9f5d20
Johannes Foslund <johannesfoslund@Johanness-MacBook-Pro.local>
2018-01-23 04:20:36 +0100

    working, moving on

--- components/gifPresent.js
+++ components/gifPresent.js
@@ -1,18 +1,18 @@
 import React from 'react'
+import FadeIn from 'react-fade-in'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
-export default class Gifpresent extends React.Component {
-
-render(){
-
-    const {title, img_url} = this.props
+export default class Gifpresent extends React.Component {
+  render() {
+    const { title, img_url } = this.props
     return (
-        
-        <div className="gif-box">
-            
-            <h2>{title}</h2><br />
-            <img src={img_url} />
+      <div className="gif-box">
+        <h2>{title}</h2>
+        <br />
 
-            <style jsx>{`
+        <img src={img_url} />
+
+        <style jsx>{`
             .gif-box{    
             background-color: white;
             border-radius:5px;
@@ -24,18 +24,33 @@ render(){
             justify-content: center;
             
           }
+
           .gif-box h2 {
               font-size: 1em;
           }
+
+          .example-enter {
+            opacity: 0.01;
+          }
+          
+          .example-enter.example-enter-active {
+            opacity: 1;
+            transition: opacity 500ms ease-in;
+          }
+          
+          .example-leave {
+            opacity: 1;
+          }
+          
+          .example-leave.example-leave-active {
+            opacity: 0.01;
+            transition: opacity 300ms ease-in;
+          }
+         
             
             
             `}</style>
-        </div>
-
+      </div>
     )
-}
-
-
-
-
+  }
 }
